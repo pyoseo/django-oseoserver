b0df05cb4c662799d976233a67751668e83aeb27
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2015-05-06 21:33:50 +0100

    fixed the management command

---
+++ oseoserver/management/commands/oseodefaults.py
@@ -0,0 +1,63 @@
+"""
+Load initial data into pyoseo
+"""
+
+from django.core.management import BaseCommand
+import oseoserver.models as models
+
+class Command(BaseCommand):
+    args = ""
+
+    help = __doc__
+
+    _order_types = (
+        "PRODUCT_ORDER",
+        "MASSIVE_ORDER",
+        "SUBSCRIPTION_ORDER",
+        "TASKING_ORDER",
+    )
+
+    _online_data_accesses = ("http", "ftp")
+
+
+    def handle(self, *args, **options):
+        item_processor = self._create_item_processor()
+        self._create_order_types(item_processor)
+        self._create_online_data_accesses()
+        group = self._create_default_oseo_group()
+        self._create_default_collection(group)
+
+    def _create_item_processor(self):
+        self.stdout.write("Creating default item processor...")
+        obj, created = models.ItemProcessor.objects.get_or_create()
+        return obj
+
+    def _create_order_types(self, item_processor):
+        self.stdout.write("Creating default order types...")
+        for t in self._order_types:
+            obj, created = models.OrderType.objects.get_or_create(
+                name=t, item_processor=item_processor)
+
+    def _create_default_oseo_group(self):
+        self.stdout.write("Creating default oseo user group...")
+        obj, created = models.OseoGroup.objects.get_or_create(
+            name="Default group",
+            authentication_class="fake.auth.class"
+        )
+        return obj
+
+    def _create_default_collection(self, authorized_group):
+        self.stdout.write("Creating default product collection...")
+        obj, created = models.Collection.objects.get_or_create(
+            name="Fake collection",
+            catalogue_endpoint="http://fake/csw/catalogue",
+            collection_id="fake_id"
+        )
+        obj.authorized_groups.add(authorized_group)
+        obj.save()
+
+    def _create_online_data_accesses(self):
+        self.stdout.write("Creating default online data access protocols...")
+        for p in self._online_data_accesses:
+            obj, created = models.OnlineDataAccess.objects.get_or_create(
+                protocol=p)
