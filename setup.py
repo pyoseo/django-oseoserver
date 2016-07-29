9ca6e1a9aeb40bfd163565e5f853cf0c33450da9
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2016-07-29 12:01:32 +0100

    Added include for the templates in MANIFEST.in

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     platforms=[""],
     license="",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "celery",
         "django",
@@ -28,5 +29,4 @@ setup(
         "pyxb",
         "redis",
     ],
-    include_package_data=True,
 )
